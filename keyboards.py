ae7ea4069c4c0a758c07d20dcbf7ce0b86e0a741
mohamadrasool0102-cell <mohamad.rasool.01.02@gmail.com>
2026-07-30 15:03:21 +0330

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,11 @@
-# NovaSmartBot
+from telegram import ReplyKeyboardMarkup
+
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        ["🤖 هوش مصنوعی", "🌍 ترجمه"],
+        ["🌤️ آب‌وهوا", "👤 پروفایل"],
+        ["ℹ️ درباره ربات", "❓ راهنما"]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=False
+)# NovaSmartBot
