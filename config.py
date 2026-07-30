56ad0cf2c7f0450da21650034cad6262234358df
mohamadrasool0102-cell <mohamad.rasool.01.02@gmail.com>
2026-07-30 15:01:47 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
 
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Weather API (بعداً مقدارش را در Render وارد می‌کنیم)
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+# AI API (بعداً مقدارش را در Render وارد می‌کنیم)
+AI_API_KEY = os.getenv("AI_API_KEY")
+
+# تنظیمات ربات
+BOT_NAME = "NovaSmartBot"
+VERSION = "1.0"
